0cc7e36053ea7348b4581bf8c115b60e9d643631
kairo-18 <gladimirdungo@gmail.com>
2025-02-28 18:06:46 +0800

    added nptc admina nd super admin role

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Middleware\NPTCAdminMiddleware;
+use App\Http\Controllers\NptcAdminController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -21,5 +22,17 @@ Route::middleware(['auth', 'verified', NPTCAdminMiddleware::class])->group(funct
         ]);
     })->name('dashboard');
 });
+
+Route::middleware('auth', 'verified', NPTCAdminMiddleware::class)->group(function () {
+    Route::post('create-nptc-admin', [NptcAdminController::class, 'createNPTCAdmin'])
+        ->name('create-nptc-admin');
+
+    Route::patch('update-nptc-admin', [NptcAdminController::class, 'updateNPTCAdmin'])
+        ->name('update-nptc-admin');
+
+    Route::delete('delete-nptc-admin', [NptcAdminController::class, 'destroy'])
+        ->name('delete-nptc-admin');
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
