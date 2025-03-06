a32267d1e34fc7497c69167e7261886bf05599bc
CharlesDors <116605757+CharlesDors@users.noreply.github.com>
2025-03-06 16:29:25 +0800

    Records, Table Component

---
+++ resources/js/components/RecordsComponent/vr-operator.tsx
@@ -0,0 +1,14 @@
+import { DataTable } from "./data-table"
+import { paymentData, paymentHeaders } from "./array-data" // Import headers
+import { generateColumns } from "./columns" // Import dynamic column generator
+
+interface CompanyProps {
+    companies: { id: number; BusinessPermitNumber: string }[];
+    onNextTab: () => void;
+}
+
+export default function Company({ companies, onNextTab }: CompanyProps) { 
+    const columns = generateColumns(paymentHeaders) // Generate columns dynamically
+
+    return <DataTable data={paymentData} columns={columns} />
+}
