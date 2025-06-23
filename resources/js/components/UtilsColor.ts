5b9f84c9b303d59f4d0da49fdda2e5716fffb9c1
crystal <66188059+kc-aquino@users.noreply.github.com>
2025-06-23 10:25:44 +0800

    Added Badge Roles in layout

--- resources/js/components/UtilsColor.ts
+++ resources/js/components/UtilsColor.ts
@@ -1,4 +1,6 @@
-// UtilsColors.ts
+// Utility functions to get colors based on user roles
+
+// getBackgroundColorForRole for the sidebar background color
 export function getBackgroundColorForRole(userRole: string | undefined): string {
     switch (userRole) {
         case 'VR Admin':
@@ -9,5 +11,23 @@ export function getBackgroundColorForRole(userRole: string | undefined): string 
             return 'bg-[#1d1d66]';
         default:
             return 'bg-[#2A2A92]';
+    }
+}
+
+// getBadgeColorForRole for the badge color in the header
+export function getBadgeColorForRole(userRole: string | undefined): string {
+    switch (userRole) {
+        case 'VR Admin':
+            return 'bg-red-600';
+        case 'Operator':
+            return 'bg-yellow-500';
+        case 'Driver':
+            return 'bg-green-600';
+        case 'Temp User':
+            return 'bg-purple-600';
+        case 'Temp User Operator':
+            return 'bg-blue-600';
+        default:
+            return 'bg-gray-600';
     }
 }
