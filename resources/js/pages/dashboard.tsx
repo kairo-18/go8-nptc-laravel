584d69225778508798b2eb4129e3401888c2038d
AlexParayno <paraynoalexander092002@gmail.com>
2025-06-23 09:48:16 +0800

    Removed Birth Date Highlight error

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -6,6 +6,7 @@ import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+
 const breadcrumbs: BreadcrumbItem[] = [
   {
     title: "Dashboard",
